5115f65f68b2245b92585091f8c9636b8cb414b6
yash joshi <yjoshi.2098@gmail.com>
2023-09-07 19:21:45 -0400

    react mantine demo

---
+++ react-mantine-modal/src/App.jsx
@@ -0,0 +1,134 @@
+// src/App.js
+import { useState } from "react";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  Modal,
+  Checkbox,
+  Button,
+  Group,
+  Switch,
+  MantineProvider,
+} from "@mantine/core";
+import "./App.css";
+
+function CheckboxComponent({ title }) {
+  return (
+    <div>
+      <div style={{ margin: "10px 0" }}>{title}:</div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <Checkbox
+            className="checkbox-wrapper"
+            checked
+            label="Linkedin Bio"
+            color="green"
+          />
+          <Checkbox
+            className="checkbox-wrapper"
+            checked
+            label="Years of experience"
+            color="green"
+          />
+          <Checkbox
+            className="checkbox-wrapper"
+            checked
+            label="Current Experience"
+            color="green"
+          />
+        </div>
+        <div>
+          <Checkbox
+            className="checkbox-wrapper"
+            label="List of past jobs"
+            color="green"
+          />
+          <div
+            style={{ display: "flex", fontSize: "14px", alignItems: "center" }}
+          >
+            <Checkbox
+              className="checkbox-wrapper"
+              disabled
+              label="Current job description"
+              color="green"
+            />
+            <div
+              style={{
+                padding: "1px 10px",
+                borderRadius: "10px",
+                color: "#cb3838",
+
+                background: "#ffe0e6",
+              }}
+            >
+              Disabled
+            </div>
+          </div>
+          <div
+            style={{ display: "flex", fontSize: "14px", alignItems: "center" }}
+          >
+            <Checkbox
+              disabled
+              className="checkbox-wrapper"
+              label="Current job specialities"
+              color="green"
+            />
+            <div
+              style={{
+                padding: "1px 10px",
+                borderRadius: "10px",
+                color: "#cb3838",
+                background: "#ffe0e6",
+              }}
+            >
+              Disabled
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const [isCentered, setIsCentered] = useState(true);
+
+  return (
+    <>
+      <MantineProvider
+        theme={{
+          fontFamily: "Verdana, sans-serif",
+          fontFamilyMonospace: "Monaco, Courier, monospace",
+          headings: { fontFamily: "Greycliff CF, sans-serif" },
+        }}
+      >
+        <Modal.Root size="xl" opened={opened} onClose={close} centered>
+          <Modal.Overlay />
+          <Modal.Content>
+            <Modal.Header style={{ background: "#40c057", color: "white" }}>
+              <Modal.Title>Personalization Settings</Modal.Title>
+              <Switch
+                color="white"
+                checked={true}
+                labelPosition="left"
+                label="Use Account Research"
+              />
+            </Modal.Header>
+            <Modal.Body>
+              <CheckboxComponent title="Profile" />
+              <hr />
+              <CheckboxComponent title="Account" />
+            </Modal.Body>
+          </Modal.Content>
+        </Modal.Root>
+
+        <Group position="center">
+          <Button onClick={open}>Open modal</Button>
+        </Group>
+      </MantineProvider>
+    </>
+  );
+}
+
+export default App;
